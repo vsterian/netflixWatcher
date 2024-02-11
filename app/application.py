e9304d363d366c6ce9f3acf508387651a8b9b9cd
Vlad Sterian <49059112+vsterian@users.noreply.github.com>
2024-02-11 23:18:01 +0200

    Update application.py

--- app/application.py
+++ app/application.py
@@ -50,6 +50,13 @@ def open_link_with_selenium(body):
                 try:
                     email_field = driver.find_element('name', 'userLoginId')
                     password_field = driver.find_element('name', 'password')
+                    message = driver.find_element(By.XPATH, '//h1[text()="This link is no longer valid"]')
+                        if message.is_displayed():
+                            return message
+                    element = WebDriverWait(driver, 10).until(message)
+                        if element:
+                            if "This link is no longer valid" in driver.page_source:
+                                print("login already made.The link is no longer valid.")
                 except NoSuchElementException:
                     print("Login fields not found. Assuming already logged in.")
                     pass
@@ -80,6 +87,7 @@ def open_link_with_selenium(body):
                         message = driver.find_element(By.XPATH, '//h1[text()="This link is no longer valid"]')
                         if message.is_displayed():
                             return message  # Return the element itself if found
+                            
                     except NoSuchElementException:
                         pass
 
